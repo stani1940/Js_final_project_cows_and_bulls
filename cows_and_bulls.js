23893a0f67266135e6bdf32b1427ceb5f78657da
stani1940 <stanislav1940@abv.bg>
2020-07-20 20:54:32 +0300

    Add function with game logic

--- cows_and_bulls.js
+++ cows_and_bulls.js
@@ -1,7 +1,7 @@
 "use strict"; //an idea more secure js code
 // Generates a non-repeating, random number and stores it in a global variable known as "secret".
 
-let numbers = [0,1,2,3,4,5,6,7,8,9];
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -13,18 +13,18 @@ function shuffleArray(array) {
     return array;
 }
 
-let secret = shuffleArray(numbers).slice(0,4).join('');
+let secret = shuffleArray(numbers).slice(0, 4).join('');
 console.log(secret);
 // This is our click counter that tells you how many times you've guessed in the game.
 
 let clicks = 0;
-$("#submit").on("click",function() {
+$("#submit").on("click", function () {
     clicks++;
     $("#guesses").text(clicks);
 });
 //add event of start button
 
-$("#start").on("click",function() {
+$("#start").on("click", function () {
     $("#game").show();
     $("#start").hide();
     $("#submit").show();
@@ -33,24 +33,66 @@ $("#start").on("click",function() {
 });
 // When you click the "Hide Rules" button, it hides the Rules form and shows the "Show Rules" button.
 
-$("#hide").on("click", function() {
+$("#hide").on("click", function () {
     $("#rulesForm").hide();
     $("#show").show();
     $("#hide").hide();
 });
 // When you click the "Show Rules" button, it shows the rules form and the "Hide Rules" button and hides the "Show Rules" button.
 
-$("#show").on("click",function() {
+$("#show").on("click", function () {
     $("#rulesForm").show();
     $("#show").hide();
     $("#hide").show();
 });
 // When you hit the "Stop playing." button, it clears the results of the game and guess input box, in addition to hiding the game functions and showing the "Start playing!" button.
 
-$("#stop").on('click',function() {
+$("#stop").on('click', function () {
     $("#game").hide();
     $("#start").show();
     $("#results").html("");
     $("#guess").val("");
     clicks = 0;
 });
+window.game = function() {
+
+    // Stores your guess in a variable
+
+    let guess = $("#guess").val();
+
+    // This is the actual game.
+
+    // These two variables will be updated with each guess the user inputs.
+
+    let bulls = 0;
+    let cows = 0;
+
+    // This is where JavaScript checks the bulls and cows and adds them up accordingly.
+
+    if (guess !== secret) {
+        if (guess.charAt(0) === secret.charAt(0)) {
+            bulls += 1;
+        } else if (guess.charAt(0) === secret.charAt(1) || guess.charAt(0) === secret.charAt(2) || guess.charAt(0) === secret.charAt(3)) {
+            cows += 1;
+        }
+        if (guess.charAt(1) === secret.charAt(1)) {
+            bulls += 1;
+        } else if (guess.charAt(1) === secret.charAt(0) || guess.charAt(1) === secret.charAt(2) || guess.charAt(1) === secret.charAt(3)) {
+            cows += 1;
+        }
+        if (guess.charAt(2) === secret.charAt(2)) {
+            bulls += 1;
+        } else if (guess.charAt(2) === secret.charAt(0) || guess.charAt(2) === secret.charAt(1) || guess.charAt(2) === secret.charAt(3)) {
+            cows += 1;
+        }
+        if (guess.charAt(3) === secret.charAt(3)) {
+            bulls += 1;
+        } else if (guess.charAt(3) === secret.charAt(0) || guess.charAt(3) === secret.charAt(1) || guess.charAt(3) === secret.charAt(2)) {
+            cows += 1;
+        }
+        $("#results").text("Bulls: " + bulls + " & Cows: " + cows + "").css("background-color","white");
+    } else if (guess === secret) {
+        $("#results").html("Congratulations, you won!").animate({'font-size':'50','margin-top':'100'},2000);
+        $("#submit").hide();
+    }
+}
